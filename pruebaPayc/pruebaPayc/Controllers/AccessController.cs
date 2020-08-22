2905dccb6259e23d452a6fb9afa2306b6605737d
unknown <jericob38@gmail.com>
2020-08-22 12:20:58 -0500

    subida de prueba incluyendo la base de datos

---
+++ pruebaPayc/pruebaPayc/Controllers/AccessController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using pruebaPayc.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace pruebaPayc.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Entrar(string email, string pass)
+        {
+            string encryptPass = Encrypt.GetSHA256(pass);
+            
+            using ( pruebaPaycEntities db = new pruebaPaycEntities())
+            {
+                var chk = from usr in db.user
+                          where usr.email == email && usr.password == encryptPass
+                          select usr;
+                if(chk.Count() > 0)
+                {
+                    return Content("1");
+                }
+                else
+                {
+                    return Content("0");
+                }
+            }
+            
+        }
+    }
+}
